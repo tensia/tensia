9193d240520c6edff8a9e0de9890b0525201cb0a
mathek <front@student.agh.edu.pl>
2017-05-27 17:21:19 +0200

    changed algoritm to use glib sets, basically should work

--- c_src/ord.c
+++ c_src/ord.c
@@ -33,87 +33,104 @@ const int de_brujin_array[64] =
 typedef struct {
   uint64_t total_cost;
   uint32_t size;
-  uint32_t origin;
+  uint64_t origin;
 } Tensor;
+
+static inline Tensor* mk_tensor(int64_t total_cost, int32_t size, int64_t origin) {
+  Tensor* t = malloc(sizeof(Tensor));
+  *t = (Tensor){.total_cost=total_cost, .size=size, .origin=origin};
+  return t;
+}
 
 void printbits(uint64_t v, char size) {
   for(int i = size-1; i >= 0; i--) putchar('0' + ((v >> i) & 1));
 }
 
-void printTensor(Tensor t) {
-  printf("cost: %lld,\tsize: %d,\torigin:\t", t.total_cost, t.size);
-  printbits(t.origin, 3);
+void print_tensor(Tensor* t) {
+  printf("cost: %lld,\tsize: %d,\torigin:\t", t->total_cost, t->size);
+  printbits(t->origin, 3);
   printf("\n");
 }
 
 static inline uint64_t contracted_dims_size(
-  uint32_t origin1, uint32_t origin2,
+  uint64_t origin1, uint64_t origin2,
   int** contracted_dims_sizes
 ) {
   uint64_t acc = 1;
-  iterate_bin(uint32_t, origin1, int i, {
-    iterate_bin(uint32_t, origin2, int j, {
+  iterate_bin(uint64_t, origin1, int i, {
+    iterate_bin(uint64_t, origin2, int j, {
       acc *= contracted_dims_sizes[i][j];
     });
   });
   return acc;
 }
 
-static inline char contractable(Tensor t1, Tensor t2) {
-  return (t1.origin & t2.origin) == 0;
+static inline char contractable(Tensor* t1, Tensor* t2) {
+  return (t1->origin & t2->origin) == 0;
 }
 
-static inline Tensor contract(
-  Tensor t1, Tensor t2, int** contracted_dims_sizes
+static inline Tensor* contract(
+  Tensor* t1, Tensor* t2, int** contracted_dims_sizes
 ) {
   uint64_t cds = contracted_dims_size(
-    t1.origin, t2.origin, contracted_dims_sizes
+    t1->origin, t2->origin, contracted_dims_sizes
   );
   // printf("%lld\n", cds);
-  return (Tensor){
-    .total_cost = max(t1.total_cost, t2.total_cost)
-      + (uint64_t)(t1.size) * t2.size / cds,
-    .size = (uint64_t)(t1.size) * t2.size / (cds * cds),
-    .origin = t1.origin | t2.origin,
-  };
+  return mk_tensor(
+    max(t1->total_cost, t2->total_cost) + (uint64_t)(t1->size) * t2->size / cds,
+    (uint64_t)(t1->size) * t2->size / (cds * cds),
+    t1->origin | t2->origin
+  );
+}
+
+static inline GHashTable* mk_hash_table() {
+  return g_hash_table_new_full(g_int_hash, g_int_equal, NULL, free);
+}
+
+static inline void reflect(GHashTable* ht, Tensor* t) {
+  Tensor* ot = (Tensor*)g_hash_table_lookup(ht, (gconstpointer)&(t->origin));
+  if(!ot || t->total_cost < ot->total_cost)
+    g_hash_table_insert(ht, (gpointer)&(t->origin), (gpointer)t);
 }
 
 uint64_t ord(int* tensors_sizes, int** contracted_dims_sizes, int tensor_cnt) {
-  Tensor* best_contr_results[tensor_cnt];
-  best_contr_results[0] = malloc(tensor_cnt*sizeof(Tensor));
+  GHashTable* best_contr_results[tensor_cnt];
+  best_contr_results[0] = mk_hash_table();
   for(int i=0; i<tensor_cnt; i++) {
-    best_contr_results[0][i] = (Tensor){
-      .total_cost=0, .size=tensors_sizes[i], .origin=1<<i,
-    };
-  }
-  uint64_t stage_sizes[tensor_cnt];
-  stage_sizes[0] = tensor_cnt;
-  for(int i=1; i<tensor_cnt; i++) {
-    stage_sizes[i] = stage_sizes[i-1]*(tensor_cnt-i)/(i+1);
-    //printf("%lld\n", stage_sizes[i]);
+    Tensor* t = mk_tensor(0, tensors_sizes[i], ((int64_t)1) << i);
+    g_hash_table_insert(best_contr_results[0], (gpointer)&(t->origin), (gpointer)t);
   }
   for(int stage=1; stage<tensor_cnt; stage++) {
-    best_contr_results[stage] = malloc(stage_sizes[stage]*sizeof(Tensor));
-    Tensor* stc = best_contr_results[stage];
-    int i = 0;
+    GHashTable* stage_content = best_contr_results[stage] = mk_hash_table();
     for(int st1=0; st1<=(stage-1)/2; st1++) {
       int st2 = stage - 1 - st1;
       printf("s: %d %d %d\n", stage, st1, st2);
-      Tensor* stc1 = best_contr_results[st1];
-      Tensor* stc2 = best_contr_results[st2];
-      for(int j=0;j<stage_sizes[st1];j++)
-        for(int k=0;k<stage_sizes[st2];k++) {
-          printTensor(stc1[j]);
-          printTensor(stc2[k]);
-          if(contractable(stc1[j], stc2[k])) {
-            stc[i++] = contract(stc1[j], stc2[k], contracted_dims_sizes);
-            printTensor(stc[i-1]);
+      GHashTableIter st1_i, st2_i;
+      g_hash_table_iter_init(&st1_i, best_contr_results[st1]);
+      Tensor *t1, *t2;
+      while(g_hash_table_iter_next(&st1_i, NULL, (gpointer*)&t1)){
+        g_hash_table_iter_init(&st2_i, best_contr_results[st2]);
+        while(g_hash_table_iter_next(&st2_i, NULL, (gpointer*)&t2)) {
+          print_tensor(t1);
+          print_tensor(t2);
+          if(contractable(t1, t2)) {
+            Tensor* c = contract(t1, t2, contracted_dims_sizes);
+            print_tensor(c);
+            reflect(stage_content, c);
           }
           printf("\n");
         }
+      }
     }
   }
-  return best_contr_results[tensor_cnt-1][0].total_cost;
+
+  Tensor* final_tensor = (Tensor*)g_list_first(g_hash_table_get_values(
+      best_contr_results[tensor_cnt-1]
+    ))->data;
+  uint64_t total_cost = final_tensor->total_cost;
+  for(int i=0; i<tensor_cnt; i++)
+    g_hash_table_destroy(best_contr_results[i]);
+  return total_cost;
 }
 
 int main() {
@@ -122,8 +139,6 @@ int main() {
   contracted_dims_sizes[0] = (int[]){0, 4, 3};
   contracted_dims_sizes[1] = (int[]){4, 0, 5};
   contracted_dims_sizes[2] = (int[]){3, 5, 0};
-//   printf("%d\n", contracted_dims_sizes[0][1]);
-// return 0;
   uint64_t res = ord(tensors_sizes, contracted_dims_sizes, 3);
   printf("%lld\n", res);
   return 0;
