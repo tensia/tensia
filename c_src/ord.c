d571e4686ed9146851dece36a1fa788a16c2162e
mathek <front@student.agh.edu.pl>
2017-05-28 01:05:04 +0200

    optimized pairing fitting tensors to contract

--- c_src/ord.c
+++ c_src/ord.c
@@ -65,9 +65,9 @@ static inline uint64_t contracted_dims_size(
   return acc;
 }
 
-static inline char contractable(Tensor* t1, Tensor* t2) {
-  return (t1->origin & t2->origin) == 0;
-}
+// static inline char contractable(Tensor* t1, Tensor* t2) {
+//   return (t1->origin & t2->origin) == 0;
+// }
 
 static inline Tensor* contract(
   Tensor* t1, Tensor* t2, int** contracted_dims_sizes
@@ -75,12 +75,26 @@ static inline Tensor* contract(
   uint64_t cds = contracted_dims_size(
     t1->origin, t2->origin, contracted_dims_sizes
   );
-  // printf("%lld\n", cds);
+  // printf("cds: %lld\n", cds);
   return mk_tensor(
     max(t1->total_cost, t2->total_cost) + (uint64_t)(t1->size) * t2->size / cds,
     (uint64_t)(t1->size) * t2->size / (cds * cds),
     t1->origin | t2->origin
   );
+}
+
+uint64_t init_bin_comb(int k) {
+  return (1 << k) - 1;
+}
+
+uint64_t next_bin_comb(int64_t comb, int n) {
+  uint64_t x = comb & -comb;
+  uint64_t y = comb + x;
+  uint64_t z = (comb & ~y);
+  comb = z / x;
+  comb >>= 1;
+  comb |= y;
+  return (comb < 1<<n) ? comb : 0;
 }
 
 static inline GHashTable* mk_hash_table() {
@@ -100,26 +114,42 @@ uint64_t ord(int* tensors_sizes, int** contracted_dims_sizes, int tensor_cnt) {
     Tensor* t = mk_tensor(0, tensors_sizes[i], ((int64_t)1) << i);
     g_hash_table_insert(best_contr_results[0], (gpointer)&(t->origin), (gpointer)t);
   }
-  for(int stage=1; stage<tensor_cnt; stage++) {
-    GHashTable* stage_content = best_contr_results[stage] = mk_hash_table();
-    for(int st1=0; st1<=(stage-1)/2; st1++) {
-      int st2 = stage - 1 - st1;
+  for(int stage=2; stage<=tensor_cnt; stage++) {
+    GHashTable* stage_content = best_contr_results[stage-1] = mk_hash_table();
+    for(int st1=1; st1<=(stage+1)/2; st1++) {
+      int st2 = stage - st1;
       printf("s: %d %d %d\n", stage, st1, st2);
-      GHashTableIter st1_i, st2_i;
-      g_hash_table_iter_init(&st1_i, best_contr_results[st1]);
-      Tensor *t1, *t2;
+      GHashTableIter st1_i;
+      g_hash_table_iter_init(&st1_i, best_contr_results[st1-1]);
+      Tensor *t1;
       while(g_hash_table_iter_next(&st1_i, NULL, (gpointer*)&t1)){
-        g_hash_table_iter_init(&st2_i, best_contr_results[st2]);
-        while(g_hash_table_iter_next(&st2_i, NULL, (gpointer*)&t2)) {
-          print_tensor(t1);
-          print_tensor(t2);
-          if(contractable(t1, t2)) {
-            Tensor* c = contract(t1, t2, contracted_dims_sizes);
-            print_tensor(c);
-            reflect(stage_content, c);
-          }
+        printf("t1: ");
+        print_tensor(t1);
+        for(
+          uint64_t comb = init_bin_comb(st2);
+          comb;
+          comb=next_bin_comb(comb, tensor_cnt-st1)
+        ) {
+          printbits(comb, 10);
           printf("\n");
+          uint64_t scaled_comb=comb;
+          for(uint64_t o1=t1->origin; o1 > 0; o1&=(o1-1)) {
+            uint64_t b = o1&(-o1);
+            scaled_comb = ((scaled_comb&~(b-1))<<1)|(scaled_comb&(b-1));
+          }
+          printbits(scaled_comb, 10);
+          printf("\n");
+          Tensor* t2 = (Tensor*)g_hash_table_lookup(
+            best_contr_results[st2-1], (gconstpointer)&scaled_comb
+          );
+          printf("t2: ");
+          print_tensor(t2);
+          Tensor* c = contract(t1, t2, contracted_dims_sizes);
+          printf("c: ");
+          print_tensor(c);
+          reflect(stage_content, c);
         }
+        printf("\n");
       }
     }
   }
